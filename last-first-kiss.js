06cefcf1232d6cac064f8b9cad4e05e02146677f
baksoy <baran.aksoy76800@gmail.com>
2024-05-14 17:19:56 +0200

    last-first-kiss.js

---
+++ last-first-kiss.js
@@ -0,0 +1,11 @@
+function first(firsteuh) {
+    return firsteuh[0];
+}
+
+function last(lasteuh) {
+    return lasteuh[lasteuh.length-1];
+}
+
+function kiss(fusion) {
+    return fusion[fusion.length-1, fusion[0]];
+}
