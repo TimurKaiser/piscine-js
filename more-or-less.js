2e8344ed0af5430470ed9f5b539addf9f72308c8
baksoy <baran.aksoy76800@gmail.com>
2024-05-14 14:47:35 +0200

    more-or-less.js

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+function more(number) {
+    return number + 1;
+}
+
+function less(number) {
+    return number - 1;
+}
+
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+function sub(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
