c3578bbdfcd32bed903fe714d8ac623733b968d2
baksoy <baran.aksoy76800@gmail.com>
2024-05-14 13:09:28 +0200

    declarations.js

---
+++ declarations.js
@@ -0,0 +1,12 @@
+const escapeSTR = "`, \, /, " '"
+const arr = [4, '2']
+const obj = {
+    str: "blabla",
+    num: 3,
+    bool: true,
+    undef: undefined,
+}
+const nested = {
+    arr: [4, undefined, '2']
+    const obj = {str:"test", num:3, bool:true };
+};
