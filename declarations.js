21bac5c95336200d139591a8df0ca01ba6e93635
baksoy <baran.aksoy76800@gmail.com>
2024-05-14 13:29:53 +0200

    declarations.js

--- declarations.js
+++ declarations.js
@@ -1,12 +1,23 @@
-const escapeSTR = "`, \, /, " '"
-const arr = [4, '2']
+// pas besoin de mettre le ; a la fin mais vaut mieux le mettre a chaque fin de déclaration
+const escapeStr = "`\"'/";
+const arr = [4, '2'];
+
+
+//méthode 1 pour le structure d'objet
 const obj = {
     str: "blabla",
     num: 3,
     bool: true,
     undef: undefined,
-}
-const nested = {
-    arr: [4, undefined, '2']
-    const obj = {str:"test", num:3, bool:true };
 };
+
+Object.freeze(obj);
+
+
+
+const nested = {
+    arr: [4, undefined, '2'], // la virgule ici sert a séparer les lignes entre les objects
+    obj: {str: "test", num: 3, bool: true } // méthode 2
+};
+
+Object.freeze(nested)
