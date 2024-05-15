10a81d9a24541a67cef81503df86400c0e2f5db5
baksoy <baran.aksoy76800@gmail.com>
2024-05-16 01:02:22 +0200

    min-max.js

---
+++ min-max.js
@@ -0,0 +1,15 @@
+function max(number1, number2) {
+    if (number1 > number2) {
+        return number1;
+    } else {
+        return number2;
+    }
+}
+
+function min(number1, number2) {
+    if (number1 > number2) {
+        return number2;
+    } else {
+        return number2;
+    }
+}
