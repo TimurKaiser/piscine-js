e8bb1908c52b8e865a447a47194e0ba69a2a3cd8
baksoy <baran.aksoy76800@gmail.com>
2024-05-16 11:26:25 +0200

    slicer.js

--- slicer.js
+++ slicer.js
@@ -6,13 +6,13 @@ function slice(input, startIndex, endIndex) {
         if (startIndex < 0) {
             startIndex = startIndex + input.length;
         }
-        if (endIndex === undefined) {
-            endIndex = input.length;
+        if (endIndex !== undefined  && endIndex < 0) {
+            endIndex = endIndex + input.length;
         }
         return input.substring(startIndex, endIndex); // substring vas aller extrare a partir de l'index start ou l'index start et end
     } else if (Array.isArray(input)) { // vaux mieux utiliser ca que typeof car l'array est un objet mais touts les objects ne sont pas des array
-        if (endIndex === undefined) {
-            endIndex = input.length;
+        if (endIndex === undefined && endIndex < 0) {
+            endIndex = endIndex + input.length;
         }
         return input.slice(startIndex, endIndex);
     } else {
