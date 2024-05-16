9bbb1787884be43d07d33976d0dc4e0145713154
baksoy <baran.aksoy76800@gmail.com>
2024-05-16 13:57:21 +0200

    keep-cut.js

--- keep-cut.js
+++ keep-cut.js
@@ -1,31 +1,31 @@
 function cutFirst(string) {
     let result = string.substring(2);
-    console.log(result)
+    return result;
 }
 
 function cutLast(string) {
     let result = string.substring(0, string.length - 2);
-    console.log(result)
+    return result;
 }
 
 function cutFirstLast(string) {
     let temp = string.substring(2);
     let result = temp.substring(0, temp.length - 2);
-    console.log(result)
+    return result;
 }
 
 function keepFirst(string) {
     let result = string.substring(0, 2);
-    return result
+    return result;
 }
 
 function keepLast(string) {
-    let result = string.substring(0, -2);
-    return result
+    let result = string.substring(string.length - 2);
+    return result;
 }
 
 function keepFirstLast(string) {
     let temp = string.substring(0, 2);
-    let result = temp.substring(0, -2);
-    return result
+    let result = temp + string.substring(string.length - 2);
+    return result;
 }
