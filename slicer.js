bec315251b46cbda90c17b09e5aef7ed5e3b858c
baksoy <baran.aksoy76800@gmail.com>
2024-05-16 11:29:18 +0200

    slicer.js

--- slicer.js
+++ slicer.js
@@ -1,18 +1,24 @@
-// fait avec gpt a revoir j'ai saturéé
-
 function slice(input, startIndex, endIndex) {
     if (typeof input === 'string') {
-
         if (startIndex < 0) {
             startIndex = startIndex + input.length;
         }
-        if (endIndex !== undefined  && endIndex < 0) {
+        if (endIndex !== undefined && endIndex < 0) {
             endIndex = endIndex + input.length;
         }
-        return input.substring(startIndex, endIndex); // substring vas aller extrare a partir de l'index start ou l'index start et end
-    } else if (Array.isArray(input)) { // vaux mieux utiliser ca que typeof car l'array est un objet mais touts les objects ne sont pas des array
-        if (endIndex === undefined && endIndex < 0) {
-            endIndex = endIndex + input.length;
+        return input.substring(startIndex, endIndex);
+    } else if (Array.isArray(input)) { // pas typeof car les array sont des objets mais les objets pas des array forcement
+        if (startIndex < 0) {
+            startIndex = input.length + startIndex;
+            if (startIndex < 0) {
+                startIndex = 0;
+            }
+        }
+        if (endIndex !== undefined && endIndex < 0) {
+            endIndex = input.length + endIndex;
+            if (endIndex < 0) {
+                endIndex = 0;
+            }
         }
         return input.slice(startIndex, endIndex);
     } else {
