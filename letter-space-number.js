c187e285c9e5c646292b3efea1b9ad1308a14efb
baksoy <baran.aksoy76800@gmail.com>
2024-05-17 16:58:34 +0200

    letter-space-number.js

---
+++ letter-space-number.js
@@ -0,0 +1,10 @@
+function letterSpaceNumber(string) {
+    // [a-zA-Z] : Correspond à une lettre (majuscule ou minuscule).
+    //** ** : Suivie par un espace.
+    //\d : Suivie par un chiffre (0-9).
+    //(?!\w) : Assure que le chiffre n'est pas suivi par un caractère de mot (lettre, chiffre ou underscore).
+    const regex = /[a-zA-Z] \d(?!\w)/g;
+    const matches = input.match(regex);
+
+    return matches ? matches : [];
+}
