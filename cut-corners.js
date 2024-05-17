49953cdb513e95d7bfed8d6462f1227cac1f2980
baksoy <baran.aksoy76800@gmail.com>
2024-05-17 11:25:07 +0200

    cut-corners.js

--- cut-corners.js
+++ cut-corners.js
@@ -1,48 +1,51 @@
 function floor(nombre) {
-    let result = nombre;
     if (nombre >= 0) {
         while (nombre >= 1) {
-            nombre -= 1; // soustrait du nombre 1 jusqu'à atteindre 0
+            nombre -= 1;
         }
-        let temp = nombre;
-        result = result - temp;
-        return result
+        return nombre + 1;
+    } else {
+        while (nombre < 0) {
+            nombre += 1;
+        }
+        return nombre - 1;
     }
-    while (nombre <= 0) {
-        nombre += 1;
-    }
-    let temp =  nombre - 1;
-    result = result - temp
-    return result
 }
+
 
 function round(nombre) {
     let temp = floor(nombre);
     let diff = nombre - temp;
     if (diff < 0.5) {
-        return temp
+        return temp;
     } else {
-        let result = nombre + (1 - diff);
-        return result
+        return temp + 1;
     }
 }
 
 
-function ceil(number) {
-    if (number > 0) {
-        let temp = floor(number);
-        let diff = number - temp;
-        let result = number + diff;
-        return result
+
+function ceil(nombre) {
+    let temp = floor(nombre);
+    if (nombre > temp) {
+        return temp + 1;
     } else {
-        return round(number)
+        return temp;
     }
 }
 
-function trunc(number) {
-    return round(number)
+function trunc(nombre) {
+    if (nombre >= 0) {
+        while (nombre >= 1) {
+            nombre -= 1;
+        }
+        return nombre + 1;
+    } else {
+        while (nombre < 0) {
+            nombre += 1;
+        }
+        return nombre - 1;
+    }
 }
-
-console.log(round(-4,6))
 
 
