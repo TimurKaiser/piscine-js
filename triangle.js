6c6f13b230dd31d6b8c17c8bb92da26636bc63e5
baksoy <baran.aksoy76800@gmail.com>
2024-05-17 15:13:23 +0200

    triangle.js

--- triangle.js
+++ triangle.js
@@ -1,6 +1,8 @@
 function triangle(string, number) {  
+  let result = "";
   for (let i = 1; i <= number; i++) {
     let ligne = string.repeat(i);
-    console.log(ligne);
+    result += ligne + "\n";
   }
+  return result;
 }
