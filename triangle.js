a091d1567b58974a8b4e540972f9471cd304672c
baksoy <baran.aksoy76800@gmail.com>
2024-05-17 14:53:41 +0200

    triangle.js

---
+++ triangle.js
@@ -0,0 +1,9 @@
+function triangle(string, number) {
+    let etg =  number;
+    let str = string;
+
+    for (var i = 1; i <= etg; i++) {
+        console.log(str)
+        console.log("\n");
+  }
+}
