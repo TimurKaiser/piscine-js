1416d758341c77daef737a0558750acd3d2fc352
baksoy <baran.aksoy76800@gmail.com>
2024-05-17 14:42:08 +0200

    get.js

--- get.js
+++ get.js
@@ -1,26 +1,11 @@
 function get(src, path) {
     const keys = path.split('.');
-    let current = src;
-    
+    let res = src;
     for (const key of keys) {
-        if (current && typeof current === 'object') {
-            if (Array.isArray(current)) {
-                const index = parseInt(key);
-                if (!isNaN(index) && index < current.length) {
-                    current = current[index];
-                } else {
-                    return undefined;
-                }
-            } else {
-                if (key in current) {
-                    current = current[key];
-                } else {
-                    return undefined;
-                }
-            }
-        } else {
+        if (res[key] === undefined) {
             return undefined;
         }
+        res = res[key];
     }
-    return current;
+    return res;
 }
