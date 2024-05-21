e9b212d126aa75bd8f493ecf808d5d0da4fff87c
baksoy <baran.aksoy76800@gmail.com>
2024-05-21 18:23:00 +0200

    sunny-sunday.js

---
+++ sunny-sunday.js
@@ -0,0 +1,22 @@
+// on change juste dr strange
+
+function addWeek(date) {
+    const baseDate = new Date('0001-01-01');
+    const dayDifference = Math.floor((date - baseDate) / (1000 * 60 * 60 * 24));
+    const newWeekDay = dayDifference % 6;
+    const days = [
+      'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+    ];
+    
+    return days[newWeekDay];
+}
+
+function timeTravel({ date, hour, minute, second }) {
+    const newDate = new Date(date);
+    
+    newDate.setHours(hour);
+    newDate.setMinutes(minute);
+    newDate.setSeconds(second);
+    
+    return newDate;
+}
