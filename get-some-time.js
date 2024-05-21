de6deebb80fb734b0a0d0514474288fdeeafc7bb
baksoy <baran.aksoy76800@gmail.com>
2024-05-21 13:03:43 +0200

    get-some-time.js

---
+++ get-some-time.js
@@ -0,0 +1,34 @@
+function firstDayWeek(numWeek, yearString) {
+    let startYear = new Date(yearString + "-01-01") 
+    let mondayCount = 1;
+
+    if (mondayCount === numWeek) {
+        return formatDate(startYear) 
+    }
+
+    while (mondayCount < numWeek) {
+        startYear.setDate(startYear.getDate() + 1);
+        if (startYear.getDay() === 1) { 
+            mondayCount++;
+        }
+        if (mondayCount === numWeek) {
+            return formatDate(startYear); 
+        }
+    }
+}
+
+
+
+function formatDate(dateString) {
+    let date = new Date(dateString);
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear().toString();
+
+
+    // formate la date a une bonne version jj--mm-aaaa
+    let formattedDate = `${day.toString().padStart(2, '0')}-${month.toString().padStart(2, '0')}-${year}`;
+
+    return formattedDate;
+}
