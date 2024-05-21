f1845e1b01a9c65684948f7341dad18568fb6e1a
baksoy <baran.aksoy76800@gmail.com>
2024-05-21 16:34:05 +0200

    count-leap-years.js

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,12 +1,28 @@
-function countLeapYears(date) {
-    const anne = date.getFullYear();
-  //easyy
-    const par4 = Math.floor(anne / 4);
-    const par100 = Math.floor(anne / 100);
-    const par400 = Math.floor(anne / 400);
-  
-    const result = par4 - par100 + par400;
-  
-    return result;
+function isLeapYear(year) {
+    let flag = false;
+
+    if (year % 4 === 0) {
+        flag = true;
+    }
+    if (year % 100 === 0) {
+        flag = false;
+    }
+    if (year % 400 === 0) {
+        flag = true;
+    }
+
+    return flag;
 }
-  
+
+function countLeapYears(date) {
+    let count = 0;
+    const currentYear = date.getFullYear();
+
+    for (let year = currentYear; year > 0; year--) {
+        if (isLeapYear(year)) {
+            count++;
+        }
+    }
+
+    return count;
+}
