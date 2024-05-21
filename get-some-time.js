30a07b9fbc6722f8697a54118b711d35c8a9b3e1
baksoy <baran.aksoy76800@gmail.com>
2024-05-21 14:18:33 +0200

    get-some-time.js

--- get-some-time.js
+++ get-some-time.js
@@ -1,36 +1,34 @@
 function firstDayWeek(numWeek, yearString) {
-    let startYear = new Date(yearString + "-01-01") 
-    let mondayCount = 1;
+    let startYear = new Date(yearString + "-01-01");
+    let mondayCount = 0;
 
-    if (mondayCount === numWeek) {
-        return formatDate(startYear) 
+    while (startYear.getDay() !== 1) {
+        startYear.setDate(startYear.getDate() + 1);
+    }
+    mondayCount = 1;
+
+    if (numWeek === 1) {
+        return formatDate(startYear);
     }
 
     while (mondayCount < numWeek) {
-        startYear.setDate(startYear.getDate() + 1);
-        if (startYear.getDay() === 1) { 
-            mondayCount++;
-        }
-        if (mondayCount === numWeek) {
-            return formatDate(startYear); 
-        }
+        startYear.setDate(startYear.getDate() + 7);
+        mondayCount++;
     }
+
+    return formatDate(startYear);
 }
 
-
-
-function formatDate(dateString) {
-    let date = new Date(dateString);
-
+function formatDate(date) {
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear().toString();
 
     if (year.length < 4) {
-        year = year.padStart();
+        year = year.padStart(4, '0');
     }
 
-    // formate la date a une bonne version jj--mm-aaaa
+    //formate a jj-mm-aa
     let formattedDate = `${day.toString().padStart(2, '0')}-${month.toString().padStart(2, '0')}-${year}`;
 
     return formattedDate;
