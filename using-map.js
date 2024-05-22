0daf1cbc3618818ec9416f5f1123ee17b4ea33c9
baksoy <baran.aksoy76800@gmail.com>
2024-05-22 11:21:53 +0200

    using-map.js

--- using-map.js
+++ using-map.js
@@ -1,45 +1,32 @@
-function citiesOnly(ville) {
-    let array = ville.map(cities => {
-        return cities.city
-    });
-    return array
+function citiesOnly(arr) {
+    return arr.map(obj => obj.city);
 }
 
-function upperCasingStates(cities) {
+function upperCasingStates(states) {
     const regex = /\b\w/g;
     return states.map(state => state.replace(regex, match => match.toUpperCase()));
 }
 
 function fahrenheitToCelsius(degrees) {
-    let array = degrees.map(temperature => {
-        const degrees = parseFloat(temperature);
-        const convert = (degrees - 32) * 5 / 9;
-
-        return  '${convert}°C';
+    return degrees.map(temperature => {
+        const fahrenheit = parseFloat(temperature);
+        const celsius = Math.floor((fahrenheit - 32) * 5 / 9);
+        return `${celsius}°C`;
     });
 }
-
 
 function trimTemp(arr) {
     const spaceRegex = /\s+/g;
-
-    return arr.map(obj => {
-        const trimmedTemperature = obj.temperature.replace(spaceRegex, '');
-        return {
-            city: obj.city,
-            temperature: trimmedTemperature
-        };
-    });
+    return arr.map(obj => ({
+        city: obj.city,
+        temperature: obj.temperature.replace(spaceRegex, '')
+    }));
 }
 
-const spaceRegex = /\s+/g;
-
-function trimTemp(arr) {
+function tempForecasts(arr) {
     return arr.map(obj => {
-        const trimmedTemperature = obj.temperature.replace(spaceRegex, '');
-
-        return {
-            ...obj, temperature: trimmedTemperature
-        };
+        const celsius = fahrenheitToCelsius([obj.temperature])[0];
+        const state = upperCasingStates([obj.state])[0];
+        return `${celsius} in ${obj.city}, ${state}`;
     });
 }
