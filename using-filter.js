5f087bba57d66eaad9ffc59a09c866e77f7b9ca4
baksoy <baran.aksoy76800@gmail.com>
2024-05-22 14:46:01 +0200

    using-filter.js

---
+++ using-filter.js
@@ -0,0 +1,34 @@
+function filterShortStateName(arr) {
+    return arr.filter(str => str.length < 7);
+}
+
+function filterStartVowel(arr) {
+    const vowelRegex = /^[aeiou]/i; // Regex pour vérifier si une chaîne commence par une voyelle
+    return arr.filter(str => vowelRegex.test(str));
+}
+
+function filter5Vowels(arr) {
+    const fiveOrMoreVowelsRegex = /([aeiou])/gi; // Regex pour compter au moins 5 voyelles
+    return arr.filter(str => {
+        const vowelsCount = str.match(fiveOrMoreVowelsRegex);
+        return vowelsCount && vowelsCount.length >= 5;
+    });
+}
+
+function filter1DistinctVowel(arr) {
+    const singleDistinctVowelRegex = new RegExp('(?![^aeiou]*[aeiou])'); // Regex pour vérifier une seule voyelle distincte
+    return arr.filter(str => {
+        const distinctVowels = Array.from(new Set(str.toLowerCase().match(singleDistinctVowelRegex)));
+        return distinctVowels.length === 1;
+    });
+}
+
+function multiFilter(arr) {
+    const vowelRegex = /^[aeiou]/i; // Réutilisation du même regex pour la comparaison avec le tag
+    return arr.filter(obj => {
+        return obj.capital.length >= 8 &&
+        !vowelRegex.test(obj.name) &&
+               vowelRegex.test(obj.tag) &&
+               obj.region!== 'South';
+    });
+}
