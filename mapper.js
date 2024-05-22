9bbd4c0e2051a71c4d01489b916f57ed14f8cea7
baksoy <baran.aksoy76800@gmail.com>
2024-05-22 23:41:54 +0200

    mapper.js

---
+++ mapper.js
@@ -0,0 +1,20 @@
+function map(array, functionn) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(functionn(array[i], i, array));
+    }
+    return result;
+}
+
+function flatMap(array, functionn) {
+    let result = [];
+    for (let i = O; i < array.length; i++) {
+        let mappedValue = fn(array[i], i, array);
+        if (Array.isArray(mappedValue)) {
+            result = result.concat(mappedValue);
+        } else {
+            result.push(mappedValue);
+        }
+    }
+    return result;
+}
