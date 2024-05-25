c8c6d24507d256fc9ab3156b247720dc4a77cd31
baksoy <baran.aksoy76800@gmail.com>
2024-05-25 13:23:23 +0200

    build-brick-and-break.js

---
+++ build-brick-and-break.js
@@ -0,0 +1,28 @@
+export function build(nBrick) {
+    let body = document.getElementsByTagName("body")[0];
+    let bricks = 1;
+    let interval = setInterval(() => {
+        let brick = document.createElement("div");
+        brick.setAttribute("id", "brick-" + bricks);
+        bricks % 3 === 2 ? (brick.dataset.foundation = true) : null;
+        body.appendChild(brick);
+        bricks++;
+        if (bricks > nBrick) {
+            clearInterval(interval);
+        }
+    }, 100);
+}
+
+export function repair(...ids) {
+    ids.forEach((id) => {
+        let brick = document.getElementById(id);
+        brick.getAttribute("foundation")
+            ? (brick.dataset.repaired = "in progress")
+            : (brick.dataset.repaired =);
+    });
+}
+
+export function destroy() {
+    let bricks = document.getElementsByTagName("div");
+    bricks[bricks.length - 1].remove();
+}
