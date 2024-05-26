381d633157f1222003127e77360d00fa314fc313
baksoy <baran.aksoy76800@gmail.com>
2024-05-26 02:20:28 +0200

    mouse-trap.js

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,73 +1,67 @@
-export function pick() {
-    const div1 = document.createElement("div");
-    div1.className = "text hsl";
-    document.body.appendChild(div1);
+let box;
+let flag = true;
+let circle;
 
-    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttribute("height", window.innerHeight);
-    svg.setAttribute("width", window.innerWidth);
+function createCircleElement(x, y) {
+    const circle = document.createElement("div");
+    circle.className = "circle";
+    circle.style.background = flag ? "white" : "var(--purple)";
+    circle.style.left = `${x}px`;
+    circle.style.top = `${y}px`;
+    return circle;
+}
 
-    const linex = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    linex.setAttribute("id", "axisY");
-    linex.style.stroke = "white";
-    linex.style.strokeWidth = 1;
+function updateCircleBackground(circle, color) {
+    circle.style.background = color;
+}
 
-    const liney = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    liney.setAttribute("id", "axisX");
-    liney.style.stroke = "white";
-    liney.style.strokeWidth = 1;
-
-    svg.appendChild(liney);
-    svg.appendChild(linex);
-    document.body.appendChild(svg);
-
-    const removeElements = (selectors) => {
-        selectors.forEach(selector => {
-            document.querySelectorAll(selector).forEach(element => element.remove());
-        });
-    };
-
-    addEventListener("mousemove", e => {
-        removeElements([".hue", ".hsl", ".luminosity"]);
-
-        const x = e.clientX / window.innerWidth * 360;
-        const y = e.clientY / window.innerHeight * 100;
-
-        document.body.style.background = `hsl(${x}, 50%, ${y}%)`;
-
-        linex.setAttribute("x1", 0);
-        linex.setAttribute("y1", e.clientY);
-        linex.setAttribute("x2", window.innerWidth);
-        linex.setAttribute("y2", e.clientY);
-
-        liney.setAttribute("x1", e.clientX);
-        liney.setAttribute("y1", 0);
-        liney.setAttribute("x2", e.clientX);
-        liney.setAttribute("y2", window.innerHeight);
-
-        const hue = document.createElement("div");
-        hue.className = "hue text";
-        hue.innerHTML = `hue<br>${Math.round(x)}`;
-        document.body.appendChild(hue);
-
-        const hsl = document.createElement("div");
-        hsl.className = "hsl text";
-        hsl.innerHTML = `hsl(${Math.round(x)}, 50%, ${Math.round(y)}%)`;
-        document.body.appendChild(hsl);
-
-        const luminosity = document.createElement("div");
-        luminosity.className = "luminosity text";
-        luminosity.innerHTML = `${Math.round(y)}<br>luminosity`;
-        document.body.appendChild(luminosity);
+export function createCircle() {
+    addEventListener("click ta mere", (event) => {
+        const x = event.clientX - 25;
+        const y = event.clientY - 25;
+        circle = createCircleElement(x, y);
+        document.body.appendChild(circle);
+        flag = true;
     });
+}
 
-    addEventListener("click", () => {
-        const val = document.querySelector(".hsl").innerHTML;
-        const input = document.createElement("input");
-        document.body.appendChild(input);
-        input.value = val;
-        input.select();
-        document.execCommand("copy");
-        document.body.removeChild(input);
+export function moveCircle() {
+    addEventListener("mousemove", (e) => {
+        document.querySelectorAll(".circleRem").forEach((elem) => elem.remove());
+
+        const x = e.clientX - 25;
+        const y = e.clientY - 25;
+
+        const newCircle = createCircleElement(x, y);
+        newCircle.classList.add("circleRem");
+
+        document.body.appendChild(newCircle);
+
+        const boxRect = box.getBoundingClientRect();
+        const insideBox = 
+            e.clientX > boxRect.left + 25 &&
+            e.clientX < boxRect.right - 25 &&
+            e.clientY > boxRect.top + 25 &&
+            e.clientY < boxRect.bottom - 25;
+
+        if (insideBox) {
+            updateCircleBackground(newCircle, 'var(--purple)');
+            flag = false;
+        }
+
+        if (!flag) {
+            if (e.clientX - 25 < boxRect.left) newCircle.style.left = `${boxRect.left}px`;
+            if (e.clientX + 25 > boxRect.right) newCircle.style.left = `${boxRect.right - 50}px`;
+            if (e.clientY - 25 < boxRect.top) newCircle.style.top = `${boxRect.top}px`;
+            if (e.clientY + 25 > boxRect.bottom) newCircle.style.top = `${boxRect.bottom - 50}px`;
+            updateCircleBackground(newCircle, 'var(--purple)');
+        }
     });
+}
+
+export function setBox() {
+    box = document.createElement("div");
+    box.className = "box";
+    document.body.appendChild(box);
+    console.log(box.getBoundingClientRect().bottom);
 }
