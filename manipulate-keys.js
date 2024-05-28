3dee891b1aa9d0c546c5d208eb1072e3f943758e
baksoy <baran.aksoy76800@gmail.com>
2024-05-28 16:27:18 +0200

    manipulate-keys.js

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -22,11 +22,12 @@ function mapKeys(obj, callback) {
 
 function reduceKeys(obj, callback, initialValue) {
     let value = initialValue || "";
-    
-    // Appliquer la fonction de réduction à chaque clé
-    Object.keys(obj).forEach(key => {
-        value += callback(value, key, initialValue) + ", ";
+
+    Object.keys(obj).sort().forEach(key => {
+        if (!value.includes(key)) {
+            value += callback(value, key, initialValue) + ", ";
+        }
     });
-    
+
     return value.slice(0, -2);
 }
