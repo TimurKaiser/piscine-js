e65df23cc3a5a7f6f06fc15255c4dd8882897741
baksoy <baran.aksoy76800@gmail.com>
2024-05-29 14:59:03 +0200

    pronoun.js

--- pronoun.js
+++ pronoun.js
@@ -1,25 +1,31 @@
-function pronoun(count, delay, callback) {
-    let attempts = 0;
-
-    return async function (...args) {
-        while (true) {
-            try {
-                const result = await Promise.race([
-                    callback(...args),
-                    new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            reject(new Error('timeout'));
-                        }, delay);
-                    })
-                ]);
-                
-                return result;
-            } catch (error) {
-                attempts++;
-                if (attempts >= count) {
-                    throw error;
-                }
-            }
-        }
+function pronoun(str) {
+    var obj = {};
+    var arr = str.split("\n").join(" ").split(" ");
+    var pronouns = /^(i|you|he|she|it|they|we)$/i;
+  
+    for (var i = 0; i < arr.length; i++) {
+      if (pronouns.test(arr[i])) {
+        var pronoun = arr[i].toLowerCase();
+        obj[pronoun] = obj[pronoun] || { count: 0, word: [] };
+        obj[pronoun].count++;
+        obj[pronoun].word.push(findNextWord(arr.slice(i)));
+      }
+    }
+  
+    for (var key in obj) {
+      obj[key].word = obj[key].word.filter((x) => x!== undefined);
+    }
+  
+    return obj;
+}
+  
+function findNextWord(arr) {
+    var pronouns = /^(i|you|he|she|it|they|we)$/i;
+    for (var i = 1; i < arr.length; i++) {
+      if (pronouns.test(arr[i])) {
+        return;
+      } else {
+        return arr[i].replace(/,/, "");
+      }
     }
 }
