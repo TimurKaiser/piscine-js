9488025cec9c01e4e03709428ae757e65075f10b
baksoy <baran.aksoy76800@gmail.com>
2024-05-29 11:55:20 +0200

    deep-copy.js

--- deep-copy.js
+++ deep-copy.js
@@ -1,23 +1,27 @@
 function deepCopy(obj) {
-
-    if( typeof obj !== 'object' || obj === null) {
+    if (typeof obj !== 'object' || obj === null) {
         return obj;
     }
-    // Si l'objet est un tableau, crée une copie du tableau en prenant récursivement chaque élément
+
+    // cas special
+    if (obj instanceof RegExp) {
+        return new RegExp(obj);
+    }
+
+
     if (Array.isArray(obj)) {
         const copyArr = [];
         for (let i = 0; i < obj.length; i++) {
-          copyArr[i] = deepCopy(obj[i]);
+            copyArr[i] = deepCopy(obj[i]);
         }
         return copyArr;
     }
-    
-    // Si l'objet est un objet, crée une copie de l'objet en copiant récursivement chaque propriété
+
     const copyObj = {};
     for (const key in obj) {
         if (obj.hasOwnProperty(key)) {
-          copyObj[key] = deepCopy(obj[key]);
+            copyObj[key] = deepCopy(obj[key]);
         }
     }
-     return copyObj;
+    return copyObj;
 }
