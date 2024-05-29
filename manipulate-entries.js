8ffe393a94dae70add02c9bc4c5642e4d0c8ada2
baksoy <baran.aksoy76800@gmail.com>
2024-05-29 11:07:38 +0200

    manipulate-entries.js

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -6,9 +6,9 @@ function mapEntries(entries, mapper) {
     return Object.fromEntries(Object.entries(entries).map(mapper));
 }
   
-function reduceEntries(entries, reducer, initialValue) {
+  function reduceEntries(entries, reducer, initialValue) {
     return Object.entries(entries).reduce(reducer, initialValue);
-}
+  }
   
 function lowCarbs(entries) {
     return filterEntries(entries, ([key, value]) => (nutritionDB[key].carbs / 100) * value <= 50);
@@ -17,17 +17,17 @@ function lowCarbs(entries) {
 function totalCalories(entries) {
     return Number(reduceEntries(entries, (acc, [key, value]) => acc + (nutritionDB[key].calories / 100) * value, 0).toFixed(1));
 }
-
-function cartTotal(entries, nutritionDB) {
+  
+function cartTotal(entries) {
     return Object.fromEntries(
-        Object.entries(entries).map(([item, amount]) => [
-            item,
-            Object.fromEntries(
-                Object.entries(nutritionDB[item]).map(([nutrient, valuePer100g]) => [
-                    nutrient,
-                    Math.round((amount / 100) * valuePer100g * 1000) / 1000
-                ])
-            )
-        ])
+      Object.entries(entries).map(([key, value]) => [
+        key,
+        Object.fromEntries(
+          Object.entries(nutritionDB[key]).map(([dbKey, dbValue]) => [
+            dbKey,
+            Math.round((value / 100) * dbValue * 1000) / 1000,
+          ])
+        ),
+      ])
     );
 }
